1433419acdce05a1403bc61ec331e33fb1fbddd2
PositronPiercer <2017ume0111@iitjammu.ac.in>
2019-04-07 16:09:16 +0700

    Added gesture login code

--- gesture.py
+++ gesture.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from skimage.measure import compare_ssim as ssim
+from pynput.keyboard import Key, Controller
 drawing = False 
 ix,iy = -1,-1
 new_gesture = []
@@ -8,6 +8,7 @@ new_gesture = []
 # mouse callback function
 def record_gesture(event,x,y,flags,param):
 	global ix,iy,drawing
+	keyboard = Controller()
 
 	if event == cv2.EVENT_LBUTTONDOWN:
 		drawing = True
@@ -17,19 +18,23 @@ def record_gesture(event,x,y,flags,param):
 		if drawing == True:
 			new_gesture.append((x,y))
 		#print('mouse moved')
+
 		
 	elif event == cv2.EVENT_LBUTTONUP:
 		drawing = False
+		keyboard.press(Key.esc)      #simulating an esc keypress to close the window
+		keyboard.release(Key.esc)
 		#print('mouse up')
 
 def store_gesture(shape):
 	passwd=np.zeros(shape,dtype=np.uint8)
 	for point in new_gesture:
-		cv2.circle(passwd,point,5,255,-1)
+		cv2.circle(passwd,point,4,255,-1)
 	cv2.imwrite('pass_ges.jpg',passwd)
 	print('New Gesture Saved')
 
 def create_new_gesture():
+	global new_gesture
 	print('Preparing to create new gesture')
 	new_gesture=[]
 	pass_img = cv2.imread('img/pass_img.png',1)
@@ -51,6 +56,7 @@ def get_mse(imageA,imageB):
 	return mse
 
 def login_gesture():
+	global new_gesture
 	new_gesture=[]
 	secret_img=cv2.imread('pass_ges.jpg',0)
 	login_img=cv2.imread('img/pass_img.png',1)
@@ -65,13 +71,35 @@ def login_gesture():
 	cv2.destroyAllWindows()
 	passwd=np.zeros(shape,dtype=np.uint8)
 	for point in new_gesture:
-		cv2.circle(passwd,point,5,255,-1)
-	similarity = ssim(passwd,secret_img)
-	print(similarity)
+		cv2.circle(passwd,point,4,255,-1)
 
-	mse=get_mse(passwd,secret_img)
-	print("Mean Sqaured Error : "+str(mse))
+	sim = compare_img(passwd,secret_img)
+
+	if sim>0.5:
+		print("Authorized ; Match : "+str(sim))
+	else:
+		print("Not Auth")
+
+
 	
+def compare_img(img1,img2): #img2 should be the secret image
+	height,width =img1.shape[:2]
+
+	white_count=0
+	original_white_count=0
+	#sim_mat=np.zeros((height,width),dtype=np.uint8)
+	for h in range(0,height):
+		for w in range(0,width):
+			if img2[h][w]==255:
+				original_white_count+=1
+			if img1[h][w]==255 and img2[h][w]==255:
+				#sim_mat[h][w]=255
+				white_count+=1
+
+
+	sim = white_count/original_white_count
+
+	return sim
 	
 
 
